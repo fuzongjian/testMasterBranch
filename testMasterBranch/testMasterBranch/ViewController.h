7de7dcf95411baa3291ca8449dc5aa98c5cda6f0
fuzongjian <fdkj0002@fdkjCSRdeMac-mini.local>
2016-02-25 16:08:09 +0800

    1.0
    测试分支合并开始

---
+++ testMasterBranch/testMasterBranch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testMasterBranch
+//
+//  Created by fdkj0002 on 16/2/25.
+//  Copyright © 2016年 FZJ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testMasterBranch/testMasterBranch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testMasterBranch
+//
+//  Created by fdkj0002 on 16/2/25.
+//  Copyright © 2016年 FZJ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
